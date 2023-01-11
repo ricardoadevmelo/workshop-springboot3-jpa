628e7630ef8fa2af650efa5bec3739cda0829a78
ricardoadevmelo <121776568+ricardoadevmelo@users.noreply.github.com>
2023-01-10 22:35:19 -0300

    H2 database, test profile, JPA

--- src/test/java/com/educandoweb/course/CourseApplicationTests.java
+++ src/test/java/com/educandoweb/course/CourseApplicationTests.java
@@ -3,7 +3,6 @@ package com.educandoweb.course;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CourseApplicationTests {
 
 	@Test
