119430947cceae33d8c7b6331215639b6586462b
ricardoadevmelo <121776568+ricardoadevmelo@users.noreply.github.com>
2023-01-14 19:36:30 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 public class User implements Serializable {
 
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
