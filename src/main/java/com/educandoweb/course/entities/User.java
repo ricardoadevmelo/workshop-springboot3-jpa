5d3a5eba0daecb72e2ee8ce89585b1bb061d78d8
ricardoadevmelo <121776568+ricardoadevmelo@users.noreply.github.com>
2023-01-29 20:53:59 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,6 +1,7 @@
 package com.educandoweb.course.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -20,6 +21,7 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
---
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.services;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.OrderRepository;
+import com.educandoweb.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll(){
+        return orderRepository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> optionalOrder = orderRepository.findById(id);
+        return optionalOrder.get();
+    }
+}
